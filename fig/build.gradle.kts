7c7e28abaabceda2e95869b6b651126d2afa76ba
theapache64 <theapache64@gmail.com>
2024-07-01 06:48:49 +0530

    ◀️ remote publish plugin

--- fig/build.gradle.kts
+++ fig/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.google.devtools.ksp") version "1.9.24-1.0.20"
-    id("com.vanniktech.maven.publish") version "0.28.0"
     kotlin("jvm") version "1.9.24"
 }
 
@@ -35,36 +34,4 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(11)
-}
-
-mavenPublishing {
-    coordinates("com.github.theapache64", "fig", "0.0.1")
-
-    // the following is optional
-
-    pom {
-        name.set("fig")
-        description.set("Google sheet based remote config library")
-        inceptionYear.set("2024")
-        url.set("https://github.com/theapache64/fig/")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        developers {
-            developer {
-                id.set("theapache64")
-                name.set("theapache64")
-                url.set("https://github.com/theapache64/")
-            }
-        }
-        scm {
-            url.set("https://github.com/theapache64/fig/")
-            connection.set("scm:git:git://github.com/theapache64/fig.git")
-            developerConnection.set("scm:git:ssh://git@github.com/theapache64/fig.git")
-        }
-    }
 }
