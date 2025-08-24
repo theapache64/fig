dee35e6e7a417236da0d6466a3a29cbfba93afa0
theapache64 <theapache64@gmail.com>
2025-08-25 01:39:30 +0530

    Use provided URL in Fig constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,12 @@ import com.github.theapache64.fig.Fig
 import com.github.theapache64.fig.FigException
 
 suspend fun main() {
-    val fig = Fig()
+    val fig = Fig("https://docs.google.com/spreadsheets/d/1LD1Su7HVzAxPlbRp9MO7lni2E5SOqfAsLMCd1FC9A8s/edit?usp=sharing")
     try {
-        fig.load(
-            sheetUrl = "https://docs.google.com/spreadsheets/d/1LD1Su7HVzAxPlbRp9MO7lni2E5SOqfAsLMCd1FC9A8s/edit?usp=sharing"
-        )
+        fig.load()
     }catch (e : FigException) {
         e.printStackTrace()
     }
 
-    println("Fruit is '${fig.getValue("fruit", null)}'")
+    println("Fruit is '${fig.getString("fruit", null)}'")
 }
